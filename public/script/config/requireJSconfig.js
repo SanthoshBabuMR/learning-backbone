36f123aa543497bc0b0d0927827ccda311eb3706
Santhosh Babu <html5webster@gmail.com>
2014-06-08 11:04:49 +0530

    update requirejs config and added Backbone API quick reference document

--- public/script/config/requireJSconfig.js
+++ public/script/config/requireJSconfig.js
@@ -1,30 +1,43 @@
-var options =  {
-	  baseUrl: "/public/script"
-	, paths : {
+/**
+* set up requirejs config
+* @function		
+* @return		{object} configuration option
+*/
+( function( ) {
+	var options =  {
+		  baseUrl: "/public/script"
+		, paths : {
 
-		// lib
-		"requireLib"			:	"lib/requirejs/require"
-		, "async"				:	"lib/requirejs/plugin/async"
-		, "text"				:	"lib/requirejs/plugin/text"
-		, "i18n"				:	"lib/requirejs/plugin/i18n"
-		, "domReady"			:	"lib/requirejs/plugin/domReady"
-		, "jquery"				:	"lib/jquery/jquery"
-		, "_"					:	"lib/underscore/underscore"
-		, "backbone"			:	"lib/backbone/backbone"
+			// lib
+			"requireLib"			:	"lib/requirejs/require"
+			, "async"				:	"lib/requirejs/plugin/async"
+			, "text"				:	"lib/requirejs/plugin/text"
+			, "i18n"				:	"lib/requirejs/plugin/i18n"
+			, "domReady"			:	"lib/requirejs/plugin/domReady"
+			, "jquery"				:	"lib/jquery/jquery"
+			, "_"					:	"lib/underscore/underscore"
+			, "backbone"			:	"lib/backbone/backbone"
 
-		// modules
-		, "main"				: 	"modules/main"
+			// modules
+			, "main"				: 	"modules/main"
+			, "todo"				:	"modules/todo"
 
-	  }
-	, shim: {
-		"backbone"				:	{
-			"exports"			:	"backbone"
-			, "deps"			:	[ "jquery", "underscore" ]	
-		}
-	  }
-	//, urlArgs : "bust=" + ( new Date() ).getTime()
+		  }
+		, shim: {
+			"_"						: {
+				"exports"			:	"_"
+			}
+			, "backbone"				:	{
+				"exports"			:	"Backbone"
+				, "deps"			:	[ "jquery", "_" ]
+			}
+		  }
+		//, urlArgs : "bust=" + ( new Date() ).getTime()
 
-};
+	};
 
-requirejs.config( options );
-return options;
+	// step: configure requirejs with the application's configuration option
+	requirejs.config( options );
+
+	return options;	
+} )( );
